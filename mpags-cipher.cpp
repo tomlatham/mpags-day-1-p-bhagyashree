0a19d66da10b184639d791bd52c149a981393746
Thomas Latham <T.Latham@warwick.ac.uk>
2019-10-17 22:02:00 +0100

    TL review of day 1 homework

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -8,44 +8,39 @@ std::string msg;
 char in_char{'x'};
 while ( std::cin >> in_char)
    {
-      if (in_char >= 'a' && in_char <='z')
+      if (isalpha(in_char))
          {
-            msg = char(toupper(in_char));//<< std::endl;
-         }
-      else if (in_char >='0' && in_char <='9')
-         {
-            switch(in_char)
-               {
-                  case '0' : msg = "ZERO";
-                     break;
-                  case '1' : msg = "ONE";
-                     break;
-                  case '2' : msg = "TWO";
-                     break;
-                  case '3' : msg = "THREE";
-                     break;
-                  case '4' : msg = "FOUR";
-                     break;
-                  case '5' : msg = "FIVE";
-                     break;
-                  case '6' : msg = "SIX";
-                     break;
-                  case '7' : msg = "SEVEN";
-                     break;
-                  case '8' : msg = "EIGHT";
-                     break;
-                  case '9' : msg = "NINE";
-                     break;
-               }
+            msg += toupper(in_char);//<< std::endl;
          }
       else
          {
-            msg = "\0" ;
+            switch(in_char)
+               {
+                  case '0' : msg += "ZERO";
+                     break;
+                  case '1' : msg += "ONE";
+                     break;
+                  case '2' : msg += "TWO";
+                     break;
+                  case '3' : msg += "THREE";
+                     break;
+                  case '4' : msg += "FOUR";
+                     break;
+                  case '5' : msg += "FIVE";
+                     break;
+                  case '6' : msg += "SIX";
+                     break;
+                  case '7' : msg += "SEVEN";
+                     break;
+                  case '8' : msg += "EIGHT";
+                     break;
+                  case '9' : msg += "NINE";
+                     break;
+               }
          }
-      std::cout << msg;
    }
 
-//std::cout << msg;////
+std::cout << msg << std::endl;
 return 0;
 }
 
